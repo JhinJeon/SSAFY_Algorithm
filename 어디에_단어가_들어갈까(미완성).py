e4a61ec7d4a8ee2be986c16e8b2046ac3f4b9f80
JeonJhin <jdznawa@naver.com>
2022-07-15 21:03:30 +0900

    금요일 스터디 2

---
+++ 어디에_단어가_들어갈까(미완성).py
@@ -0,0 +1,37 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    n, k = map(int,input().split())
+    puzzle = []
+    for i in range(n):
+        puzzle.append(list(map(int,input().split())))
+    answer = 0
+    streak = 0
+    for i in range(n):	#가로 인덱스
+        streakmax = []
+        for j in range(n):
+            if puzzle[i][j] == 1:
+                streak += 1
+            else:
+                streak = 0
+            if streak >= k:
+                streakmax.append(streak)
+        for m in streakmax:
+            if max(streakmax) != k:
+                break
+            if m == k:
+                answer += 1
+    for i in range(n):
+        streakmax = []
+        for j in range(n):  #세로 인덱스
+            if puzzle[j][i] == 1:
+                streak += 1
+            else:
+                streak = 0
+            if streak >= k:
+                streakmax.append(streak)
+        for m in streakmax:
+            if max(streakmax) != k:
+                break
+            if m == k:
+                answer += 1                 
+    print("#"+str(test_case)+" "+str(answer))
---
+++ 파리퇴치(미완성).py
@@ -0,0 +1,17 @@
+t = int(input())
+n,m = map(int,input().split())
+for k in range(t):
+    answer = 0
+    area = []
+    for i in range(n):
+        area.append(list(map(int,input().split())))
+    if m == n:	#파리채가 전 범위를 커버하는 경우
+        for i in range(len(sum)):
+            answer += sum(area[i])
+    else:	#파리채로 잡는 부분 미구현
+        catch = 0
+        catchsum = []
+        for dy in range(n-m):
+            for dx in range(n-m):
+                catch += area[dy][dx]
+print(&apos;#&apos;+str(t+1)+&apos; &apos;+str(answer))
