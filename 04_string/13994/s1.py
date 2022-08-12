e88e6b9ddfb0c2efc2418d7e3ba9dc78c5621dd6
JhinJeon <jdznawa@naver.com>
2022-08-12 17:56:07 +0900

    0812 extra 문제 포함 완료

--- 04_string/13994/s1.py
+++ 04_string/13994/s1.py
@@ -10,39 +10,38 @@ t = int(input())
 for t in range(1, t + 1):
     stops = list()
     n = int(input())
+    end_lim = 0
     for i in range(n):
         bustype, start, end = map(int,input().split())
+        if end > end_lim:
+            end_lim = end
         if bustype == 1:
             stops.append(list(range(start,end+1)))
         elif bustype == 2:
             temp_2 = list()
-            temp_2.append(start)
-            temp_2.append(end)
             if start % 2 == 1:
-                for k in range(start+1,end):
+                for k in range(start,end + 1):
                     if k % 2 == 1:
                         temp_2.append(k)
             else:
-                for k in range(start+1,end):
+                for k in range(start,end + 1):
                     if k % 2 == 0:
                         temp_2.append(k)
             stops.append(temp_2)
         else:
             temp_3 = list()
-            temp_3.append(start)
-            temp_3.append(end)
             if start % 2 == 0:
-                for k in range(start+1,k):
+                for k in range(start, end+1):
                     if k % 4 == 0:
                         temp_3.append(k)
             else:
-                for k in range(start+1,k):
+                for k in range(start,end + 1):
                     if k % 3 == 0 and k % 10 != 0:
                         temp_3.append(k)
             stops.append(temp_3)
 
     answer = 0
-    for i in range(1000):
+    for i in range(end_lim):
         common = 0
         for stop in stops:
             if i in stop:
