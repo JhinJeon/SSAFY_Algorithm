007104e801d6ddc81aff82860c0a518c51073e8c
DongJun <jdznawa@naver.com>
2022-09-23 18:00:48 +0900

    0923 실습내용 커밋

--- 19_A/1952/s2.py
+++ 19_A/1952/s2.py
@@ -3,23 +3,26 @@ import sys
 sys.stdin = open('sample_input.txt')
 
 
-def dfs(start, k, case_sum):
-    global monthly_plan, fee_case
+def dfs(k, case_sum):
+    global monthly_plan, fee_sum, start
+    result = start + case_sum
     if k >= 12:
-        fee_case.append(sum(monthly_plan[:start]) + case_sum)
+        if fee_sum > result:
+            fee_sum = result
         return
     for i in range(k, 12):
         if fee_plan[i]:
             if sum(fee_plan[i:i + 3]) > quarter:
                 case_sum += quarter
-                dfs(start, k+3, case_sum)
+                dfs(k+3, case_sum)
                 case_sum -= quarter
             else:
                 case_sum += fee_plan[i]
-                dfs(start, k+1, case_sum)
+                dfs(k+1, case_sum)
                 case_sum -= fee_plan[i]
     else:
-        fee_case.append(sum(monthly_plan[:start]) + case_sum)
+        if fee_sum > result:
+            fee_sum = result
         return
 
 
@@ -29,9 +32,9 @@ for tc in range(1,t+1):
     # 수영장 요금(1일, 1주, 3달, 1년)
     daily, monthly, quarter, annual = map(int,input().split())
     # 수영장 이용 계획
+    fee_sum = max(daily, monthly, quarter, annual) * 12
     monthly_plan = list(map(int,input().split()))
     fee_plan = [0] * 14
-    fee_sum = 0
 
     for m in range(12):
         playtime = monthly_plan[m]
@@ -41,12 +44,10 @@ for tc in range(1,t+1):
         else:
             fee_plan[m] = daily_fee
 
-    fee_case = []
-
     for i in range(12):
-        dfs(i, i, 0)
+        start = sum(fee_plan[:i])
+        dfs(i, 0)
 
-    fee_sum = min(fee_case)
     if fee_sum > annual:
         fee_sum = annual
 
