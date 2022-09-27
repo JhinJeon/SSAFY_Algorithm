a65f5348a2204ec6d1742128f8adc5b6188da694
DongJun <jdznawa@naver.com>
2022-09-27 17:52:24 +0900

    0927 실습 중간커밋

--- 21/1249/s1.py
+++ 21/1249/s1.py
@@ -9,18 +9,18 @@ dx = [0, 1, 0, -1]
 
 def bfs(col, row):
     global answer
-    next_visit =[(0, 0, 0, 0, 0)]        # 다음 방문 지점(기본값 = 원점 기준)
+    next_visit =[(0, 0, 0, 0, 0)]   # 다음 방문 지점(행, 열, 평탄화 값, 이전 행, 이전 열)
     while next_visit:
         for _ in range(len(next_visit)):
-            x, y, dig_total, last_y, last_x = next_visit.pop(0)
+            y, x, dig_total, y_before, x_before = next_visit.pop(0)
             # 개선의 여지가 없는 경우 탐색 중단(백트래킹)
             if dig_total > answer:
                 break
             for direction in range(4):
                 nx = x + dx[direction]
                 ny = y + dy[direction]
-                # 왔던 방향에서 되돌아가는 경우
-                if dx[direction] == -last_x and dy[direction] == -last_y:
+                # 왔던 방향으로 되돌아가는 경우
+                if ny == y_before and nx == x_before:
                     continue
                 # 목적지에 도달한 경우
                 if ny == n-1 and nx == n-1:
@@ -29,7 +29,7 @@ def bfs(col, row):
                     break
                 # 목적지를 향해서 경로를 찾는 경우
                 elif 0 <= nx < n and 0 <= ny < n:
-                    next_visit.append((ny, nx, dig_total + graph[ny][nx], dy[direction], dx[direction]))
+                    next_visit.append((ny, nx, dig_total + graph[ny][nx], y, x))
 
 
 t = int(input())
