7f248fdaeaa8a53920d7e5e3292a21d97214a1ea
JeonJhin <jdznawa@naver.com>
2022-09-30 01:45:26 +0900

    0930 실습내용 복습

--- 23/5249/s1.py
+++ 23/5249/s1.py
@@ -1,14 +1,16 @@
+# 최소 신장 트리
 import sys
 sys.stdin = open('sample_input.txt')
 
 
 def prim(start):
-    visited = [False] * (n+1)
+    visited = [False] * (n+1)   # 방문 여부
     distance = [INF] * (n+1)
-    distance[start] = 0
+    distance[start] = 0         # distance = [인덱스 번호]노드로 향하는 최단 거리들의 정보
+    cost = 0
 
     # 정점의 개수만큼 반복하며 모든 정점을 이은 MST 생성
-    for _ in range(n+1):
+    for _ in range(n):
         # 1. MST의 정점에서 이동할 수 있는 모든 정점들 중 가장 적은 비용으로 이동 가능한 정점 찾기(그리디)
         min_dist = INF
         for i, dist in enumerate(distance):
@@ -17,14 +19,17 @@ def prim(start):
                 min_node = i
                 min_dist = dist
 
-        # 2. 해당 정점을 MST에 포함
+        # 2. 해당 정점을 MST에 포함한 후 비용 합산
         visited[min_node] = True
+        cost += min_dist
 
         # 3. 해당 정점과 인접한 정점에 대해 최소 비용 갱신
         for next_node, dist in graph[min_node]:
             if not visited[next_node] and dist < distance[next_node]:
                 distance[next_node] = dist
-                fastest[next_node] = dist
+
+    # 모두 계산한 경우 최단 거리들의 합계 반환
+    return sum(distance)
 
 t = int(input())
 
@@ -32,12 +37,11 @@ for tc in range(1,t+1):
     n, m = map(int, input().split())
     graph = [[] for _ in range(n+1)]
     INF = 99999999          # 임의의 큰 수
-    fastest = [INF] * (n+1)
 
     for _ in range(m):
         s, e, w = map(int, input().split())
         graph[s].append((e, w))
+        graph[e].append((s, w))
 
-    prim(0)
-    answer = sum(fastest[1:])
-    print(f'#{tc}')
+    # 0번 노드부터 탐색
+    print(f'#{tc} {prim(0)}')
--- 23/5251/s1.py
+++ 23/5251/s1.py
@@ -1,45 +1,52 @@
+# 최소 이동 거리
+
 import sys
 sys.stdin = open('sample_input.txt')
 
 
-def prim(start):
-    visited = [False] * (n+1)
-    distance = [INF] * (n+1)
+# 현재 위치를 기준으로 각 노드까지의 최단 거리를 반환하는 함수 dijkstra
+def dijkstra(start):
+    visited = [False] * (n + 1)
+    visited[start] = True
     distance[start] = 0
-    cost = 0
 
-    # 정점의 개수만큼 반복하며 모든 정점을 이은 MST 생성
+    # 최단 거리 초기화
+    for e, w in graph[start]:
+        distance[e] = w
+
+    # 시작 정점을 제외한 나머지 정점에 대해 반복
     for _ in range(n):
         # 1. MST의 정점에서 이동할 수 있는 모든 정점들 중 가장 적은 비용으로 이동 가능한 정점 찾기(그리디)
         min_dist = INF
-        for i, dist in enumerate(distance):
-            # 더 작은 값을 발견한 경우 해당 값으로 수정
-            if not visited[i] and dist < min_dist:
+        for i in range(1, n+1):
+            # 아직 방문하지 않은 노드 방향의 비용이 더 작은 값을 발견한 경우 해당 값으로 수정
+            if not visited[i] and distance[i] < min_dist:
                 min_node = i
-                min_dist = dist
+                min_dist = distance[i]
 
         # 2. 해당 정점을 MST에 포함한 후 비용 합산
         visited[min_node] = True
-        cost += min_dist
 
         # 3. 해당 정점과 인접한 정점에 대해 최소 비용 갱신
         for next_node, dist in graph[min_node]:
-            if not visited[next_node] and dist < distance[next_node]:
-                distance[next_node] = dist
-
-    return cost
+            new_dist = distance[min_node] + dist
+            if new_dist < distance[next_node]:
+                distance[next_node] = new_dist
 
 
 t = int(input())
+INF = 99999999          # 임의의 큰 수
 
-for tc in range(1,t+1):
+for tc in range(1, t+1):
     n, m = map(int, input().split())
-    graph = [[] for _ in range(n+2)]
-    INF = 99999999          # 임의의 큰 수
+    graph = [[] for _ in range(m)]      # 연결 정보
+    distance = [INF] * (n+1)            # 거리 정보
 
     for _ in range(m):
         s, e, w = map(int, input().split())
         graph[s].append((e, w))
 
-    answer = prim(0)
-    print(f'#{tc}', answer)
+    # 0번 지점을 기준으로 각 지점까지의 최단거리 탐색
+    dijkstra(0)
+    # 끝(n번) 지점까지의 거리만 출력
+    print(f'#{tc} {distance[n]}')
